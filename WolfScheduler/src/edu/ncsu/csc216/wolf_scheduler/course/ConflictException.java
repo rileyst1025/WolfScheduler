e043307f4ab53d01e889fdb2825a23a30f2a84ad
riley <riley@DESKTOP-3QBIQEM>
2021-02-15 15:57:25 -0600

    [Implementation][Test] Completed functionality and test for CourseExecption

--- WolfScheduler/src/edu/ncsu/csc216/wolf_scheduler/course/ConflictException.java
+++ WolfScheduler/src/edu/ncsu/csc216/wolf_scheduler/course/ConflictException.java
@@ -10,4 +10,21 @@ package edu.ncsu.csc216.wolf_scheduler.course;
  */
 public class ConflictException extends Exception {
 
+	/** ID used for serialization. */
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * Creates a Conflict Exception with the message passed to it
+	 * @param message the message to display when Conflict Exception is called
+	 */
+	public ConflictException(String message) {
+		super(message);
+	}
+	
+	/**
+	 * Creates a Conflict Exception with the default message "Schedule conflict"
+	 */
+	public ConflictException() {
+		super("Schedule conflict");
+	}
 }
---
+++ WolfScheduler/src/edu/ncsu/csc216/wolf_scheduler/course/ConflictExceptionTest.java
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+package edu.ncsu.csc216.wolf_scheduler.course;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * Tests Conflict Execption both with and without a parameter message
+ * @author rsthoma5
+ *
+ */
+public class ConflictExceptionTest {
+
+	/**
+	 * Tests ConflictExecption with a parameter
+	 */
+	@Test
+	public void testConflictExceptionString() {
+		ConflictException ce = new ConflictException("Custom exception message");
+	    assertEquals("Custom exception message", ce.getMessage());
+	}
+
+	/**
+	 * Tests ConflictException without a parameter
+	 */
+	@Test
+	public void testConflictException() {
+		ConflictException ce = new ConflictException();
+	    assertEquals("Schedule conflict", ce.getMessage());
+	}
+
+}
