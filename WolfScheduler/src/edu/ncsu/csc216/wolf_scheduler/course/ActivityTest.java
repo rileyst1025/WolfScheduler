3aa1d01bab70765ddb9abd3c9455d7e8e207d67d
riley <riley@DESKTOP-3QBIQEM>
2021-02-15 19:45:33 -0600

    [Implementation][Test] Completed functionality for checkConflict and its Test Class

--- WolfScheduler/src/edu/ncsu/csc216/wolf_scheduler/course/ActivityTest.java
+++ WolfScheduler/src/edu/ncsu/csc216/wolf_scheduler/course/ActivityTest.java
@@ -25,6 +25,7 @@ public class ActivityTest {
 	        a1.checkConflict(a2);
 	        assertEquals("Incorrect meeting string for this Activity.", "MW 1:30PM-2:45PM", a1.getMeetingString());
 	        assertEquals("Incorrect meeting string for possibleConflictingActivity.", "TH 1:30PM-2:45PM", a2.getMeetingString());
+	        a2.checkConflict(a1);
 	    } catch (ConflictException e) {
 	        fail("A ConflictException was thrown when two Activities at the same time on completely distinct days were compared.");
 	    }
@@ -43,6 +44,30 @@ public class ActivityTest {
 	    } catch (ConflictException e) {
 	        assertEquals("Incorrect meeting string for this Activity.", "MW 1:30PM-2:45PM", a1.getMeetingString());
 	        assertEquals("Incorrect meeting string for possibleConflictingActivity.", "M 1:30PM-2:45PM", a2.getMeetingString());
+	    }
+	}
+	
+	/**
+	 * Tests to see if ConflictException is thrown with end time the same as start time and vice versa
+	 */
+	@Test
+	public void testCheckConflictWithBackToBackTimes() {
+		Activity a1 = new Course("CSC 216", "Software Development Fundamentals", "001", 3, "sesmith5", "MW", 1330, 1445);
+	    Activity a2 = new Course("CSC 216", "Software Development Fundamentals", "001", 3, "sesmith5", "M", 1445, 1530);
+	    try {
+	        a1.checkConflict(a2);
+	        fail("A ConflictException was NOT thrown when two Activities had a day/time conflict.");
+	    } catch (ConflictException e) {
+	        assertEquals("Incorrect meeting string for this Activity.", "MW 1:30PM-2:45PM", a1.getMeetingString());
+	        assertEquals("Incorrect meeting string for possibleConflictingActivity.", "M 2:45PM-3:30PM", a2.getMeetingString());
+	    }
+	    a2.setMeetingDaysAndTime("M", 1230, 1330);
+	    try {
+	        a1.checkConflict(a2);
+	        fail("A ConflictException was NOT thrown when two Activities had a day/time conflict.");
+	    } catch (ConflictException e) {
+	        assertEquals("Incorrect meeting string for this Activity.", "MW 1:30PM-2:45PM", a1.getMeetingString());
+	        assertEquals("Incorrect meeting string for possibleConflictingActivity.", "M 12:30PM-1:30PM", a2.getMeetingString());
 	    }
 	}
 }
